b95c91f7c466b99531f34c07a0a913498eee1724
Henry Chung <chungthehao@gmail.com>
2020-01-20 06:03:13 +0700

    Laravel Routing & Vue Routing

---
+++ resources/js/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>App</h1>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+};
+</script>
+
+<style lang="scss" scoped></style>
