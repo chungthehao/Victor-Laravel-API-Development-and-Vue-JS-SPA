90bb8889722cc69fc32f91257ef53106c152529f
Henry Chung <chungthehao@gmail.com>
2020-01-20 12:11:48 +0700

    Test Cleanup & Refactor

--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -11,18 +11,20 @@ class ContactsTest extends TestCase
 {
     use RefreshDatabase;
 
-    /** @test */
-    public function a_contact_can_be_added()
+    private function data()
     {
-        // Không có dòng này thì Laravel sẽ handle exception nên mình ko bị lỗi báo endpoint ko tồn tại
-        $this->withoutExceptionHandling();
-
-        $this->post('/api/contacts', [
+        return [
             'name' => 'Len Chay Hu',
             'email' => 'hu@1.com',
             'birthday' => '08/17/1992',
             'company' => 'ABC Company'
-        ]);
+        ];
+    }
+
+    /** @test */
+    public function a_contact_can_be_added()
+    {
+        $this->post('/api/contacts', $this->data());
 
         $contact = Contact::first();
 
@@ -34,42 +36,26 @@ class ContactsTest extends TestCase
     }
 
     /** @test */
-    public function a_name_is_required()
+    public function fields_are_required()
     {
-        // $this->withoutExceptionHandling(); // Comment vì validation fail thì Laravel quăng exception: The given data was invalid. -> nó có validate cho mình, để nó handle exception để trả response là validation fail rồi cho mình (expect). -> Khi đó test sẽ xanh
-
-        $response = $this->post('/api/contacts', [
-            'email' => 'hu@1.com',
-            'birthday' => '08/17/1992',
-            'company' => 'ABC Company'
-        ]);
-
-        $contact = Contact::first();
-
-        // Khi insert ko có 'name', phải báo lỗi validation
-        $response->assertSessionHasErrors('name');
-
-        // Và khi validation fail, không có record nào đc insert
-        $this->assertCount(0, Contact::all());
+        collect(['name', 'email', 'birthday', 'company'])
+            ->each(function ($field) {
+                $response = $this->post(
+                    '/api/contacts', 
+                    array_merge(
+                        $this->data(),
+                        [$field => '']
+                    )
+                );
+        
+                $contact = Contact::first();
+        
+                // Khi insert ko có 'name', phải báo lỗi validation
+                $response->assertSessionHasErrors($field);
+        
+                // Và khi validation fail, không có record nào đc insert
+                $this->assertCount(0, Contact::all());
+            });
     }
 
-    /** @test */
-    public function email_is_required()
-    {
-        // $this->withoutExceptionHandling(); // Comment vì validation fail thì Laravel quăng exception: The given data was invalid. -> nó có validate cho mình, để nó handle exception để trả response là validation fail rồi cho mình (expect). -> Khi đó test sẽ xanh
-
-        $response = $this->post('/api/contacts', [
-            'name' => 'Hao Chung',
-            'birthday' => '08/17/1992',
-            'company' => 'ABC Company'
-        ]);
-
-        $contact = Contact::first();
-
-        // Khi insert ko có 'name', phải báo lỗi validation
-        $response->assertSessionHasErrors('email');
-
-        // Và khi validation fail, không có record nào đc insert
-        $this->assertCount(0, Contact::all());
-    }
 }
